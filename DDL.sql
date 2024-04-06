5ac6e6acf85d8bfe82cd760dc30f9353ca8e65f8
Higashikata <96960411+Highashikata@users.noreply.github.com>
2024-04-06 03:05:28 +0200

    Add files via upload

---
+++ DDL.sql
@@ -0,0 +1,10 @@
+drop table if exists Q4_data;
+create table Q4_data
+(
+	id			int,
+	name		varchar(20),
+	location	varchar(20)
+);
+
+
+select * from Q4_data;
---
+++ DML.sql
@@ -0,0 +1,5 @@
+insert into Q4_data values(1,null,null);
+insert into Q4_data values(2,'David',null);
+insert into Q4_data values(3,null,'London');
+insert into Q4_data values(4,null,null);
+insert into Q4_data values(5,'David',null);
